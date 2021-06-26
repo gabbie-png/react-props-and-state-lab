49c6aed35fdccad3f840de1cbb47ca731b84872f
Gabriella RoseMarino <Gabriellarosemarino@gmail.com>
2021-06-26 11:58:17 -0400

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -4,17 +4,17 @@ class Filters extends React.Component {
 
 
 
- handleChange = e => {
-   this.props.onChangeType(e.target.value)
- }
+//  handleChange = e => {
+//    this.props.onChangeType(e.target.value)
+//  }
 
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type" value={this.props.filters.type}
-          onChange={this.handleChange}>
+          <select name="type" id="type" 
+          onChange={this.props.onChangeType}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
